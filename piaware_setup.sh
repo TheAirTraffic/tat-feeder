97a37ab4d804f23dca416bb3dbfe84cc659321ca
Joe Prochazka <jprochazka@users.noreply.github.com>
2015-11-03 10:52:10 -0500

    No longer use wget to obtain theairtraffic-maint.sh.

--- piaware_setup.sh
+++ piaware_setup.sh
@@ -9,9 +9,11 @@ echo "-----------------------------------------------------"
 echo -e "\033[33m"
 echo "TheAirTraffic.com is a co-op of ADS-B/Mode S/MLAT feeders from around the world."
 echo "This script will setup your current PiAware installation to share your data with TheAirTraffic as well."
-echo "Please note that PiAware is required to be installed in order to feed TheAirTraffic."
+echo "PiAware is required to be installed in order to use this script to setup the feed to TheAirTraffic."
 echo ""
-echo "http://www.theairtraffic.com"
+echo "https://github.com/jprochazka/TheAirTraffic"
+echo "http://www.theairtraffic.com/how-to-feed/"
+echo "http://www.theairtraffic.com/forums/topic/TheAirTraffic-setup-script/"
 echo -e "\033[37m"
 read -p "Continue setup? [Y/n] " CONTINUE
 
@@ -21,33 +23,9 @@ if [[ ! $CONTINUE =~ ^[Yy]$ ]]; then
     exit 0
 fi
 
-## SPECIFY THE DIRECTORY TO PLACE THEAIRTRAFFIC MAINTAINANCE SCRIPT
+## ASSIGN THE SCRIPTDIR VARIABLE WHICH SHOULD BE THE DIRECTORY CONTAINING THIS SCRIPT
 
-echo -e "\033[33m"
-echo "Please specify a directory in which to download the TheAirTraffic maintainance script."
-echo "This directory will be created for you if it does not already exist."
-echo -e "\033[37m"
-
-NOBUILDDIR="true"
-while [[ $NOBUILDDIR == 'true' ]]
-do
-    read -p "Directory Path: [$PWD/TheAirTraffic] " BUILDDIR
-    if [[ $BUILDDIR == '' ]]; then
-        BUILDDIR="$PWD/TheAirTraffic"
-    fi
-    if [ ! -d "$BUILDDIR" ]; then
-        echo ""
-        mkdir -p $BUILDDIR
-    fi
-    if [ -d "$BUILDDIR" ]; then
-        NOBUILDDIR="false"
-    else
-        echo "Please make sure the path specified is valid and you have permission to write to it."
-        echo -e "\033[37m"
-    fi
-done
-
-cd $BUILDDIR
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ## CONFIGURE PIAWARE TO FEED THEAIRTRAFFIC
 
@@ -63,40 +41,33 @@ COMMAND=`sudo piaware-config -mlatResultsFormat "${CLEANFORMAT} beast,connect,fe
 $COMMAND
 sudo piaware-config -restart
 
-## DOWNLOAD THE THEAIRTRAFFIC MAINTAINANCE SCRIPT
-
-echo -e "\033[33m"
-echo "Downloading TheAirTraffic maintainance script..."
-echo -e "\033[37m"
-mkdir $BUILDDIR/
-wget http://bucket.theairtraffic.com/theairtraffic-maint.sh -O $BUILDDIR/theairtraffic-maint.sh
-
 ## SET PERMISSIONS ON THE THEAIRTRAFFIC MAINTAINANCE SCRIPT
 
 echo -e "\033[33mSetting permissions on the TheAirTraffic maintainance script..."
 echo -e "\033[37m"
-sudo chmod 755 $BUILDDIR/theairtraffic-maint.sh
+sudo chmod 755 $SCRIPTDIR/theairtraffic-maint.sh
 
 ## ADD THEAIRTRAFFIC MAINTAINANCE SCRIPT TO RC.LOCAL
 
-if ! grep -Fxq "${BUILDDIR}/theairtraffic-maint.sh &" /etc/rc.local; then
+if ! grep -Fxq "${SCRIPTDIR}/theairtraffic-maint.sh &" /etc/rc.local; then
     echo -e "\033[33mAdding TheAirTraffic maintainance script startup command to rc.local..."
     echo -e "\033[37m"
     lnum=($(sed -n '/exit 0/=' /etc/rc.local))
-    ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i ${BUILDDIR}/theairtraffic-maint.sh &\n" /etc/rc.local
+    ((lnum>0)) && sudo sed -i "${lnum[$((${#lnum[@]}-1))]}i ${SCRIPTDIR}/theairtraffic-maint.sh &\n" /etc/rc.local
 fi
 
 ## RUN THE THEAIRTRAFFIC MAINTAINANCE SCRIPT
 
 echo -e "\033[33mRunning TheAirTraffic maintainance script..."
 echo -e "\033[37m"
-sudo $BUILDDIR/theairtraffic-maint.sh start &
+sudo $SCRIPTDIR/theairtraffic-maint.sh start &
 
 ## DISPLAY SETUP COMPLETE MESSAGE
 
 echo -e "\033[33m"
 echo "Configuration of the TheAirTraffic feed is now complete."
 echo "Please look over the output generated to be sure no errors were encountered."
+echo "Also make sure to leave the files and folders contained here in place."
 echo -e "\033[37m"
 read -p "Press enter to exit this script..." CONTINUE
 echo ""
