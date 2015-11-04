8b3c39b9d952d1127f68b19c0d71398285cd5113
Joe Prochazka <jprochazka@users.noreply.github.com>
2015-11-04 00:41:07 -0500

    Removed a few not needed "commands".

--- piaware_setup.sh
+++ piaware_setup.sh
@@ -45,7 +45,7 @@ sudo piaware-config -restart
 
 echo -e "\033[33mSetting permissions on the TheAirTraffic maintainance script..."
 echo -e "\033[37m"
-sudo chmod 755 $SCRIPTDIR/theairtraffic-maint.sh
+chmod 755 $SCRIPTDIR/theairtraffic-maint.sh
 
 ## ADD THEAIRTRAFFIC MAINTAINANCE SCRIPT TO RC.LOCAL
 
@@ -70,7 +70,7 @@ fi
 
 echo -e "\033[33mRunning TheAirTraffic maintainance script..."
 echo -e "\033[37m"
-sudo $SCRIPTDIR/theairtraffic-maint.sh start &
+sudo $SCRIPTDIR/theairtraffic-maint.sh &
 
 ## DISPLAY SETUP COMPLETE MESSAGE
 
