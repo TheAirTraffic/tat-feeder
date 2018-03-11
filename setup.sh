572b24c1de7602b56c71ca806edda159bccc9253
TheAirTraffic <noreply@theairtraffic.com>
2018-03-11 09:51:44 -0700

    SOCAT over NC
    /bin/socat -u TCP:localhost:30005 TCP:feed.theairtraffic.com:$RECEIVERPORT

--- setup.sh
+++ setup.sh
@@ -224,7 +224,8 @@ EOF
 while true
   do
     sleep 30
-    /bin/nc 127.0.0.1 30005 | /bin/nc feed.theairtraffic.com $RECEIVERPORT
+    #/bin/nc 127.0.0.1 30005 | /bin/nc feed.theairtraffic.com $RECEIVERPORT
+    /bin/socat -u TCP:localhost:30005 TCP:feed.theairtraffic.com:$RECEIVERPORT
   done
 EOF
 
