31e97fa65bd5008f0fa32fafee6db4cd08165270
TheAirTraffic <noreply@theairtraffic.com>
2020-02-11 23:47:59 -0700

    Update theairtraffic-feed.sh

--- scripts/adsbexchange-feed.sh
+++ scripts/adsbexchange-feed.sh
@@ -8,7 +8,8 @@ do
 	if ping -q -c 2 -W 5 feed.theairtraffic.com >/dev/null 2>&1
 	then
 		echo Connected to feed.theairtraffic.com:$SERVERPORT
-		/usr/bin/socat -u TCP:$INPUT TCP:feed.theairtraffic.com:$SERVERPORT
+		/usr/local/bin/feed-tat
+		#/usr/bin/socat -u TCP:$INPUT TCP:feed.theairtraffic.com:$SERVERPORT
 		echo Disconnected, reconnecting in 30 seconds!
 	else
 		echo Unable to connect to feed.theairtraffic.com, trying again in 30 seconds!
