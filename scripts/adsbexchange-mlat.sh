fdae3aa254f7c3ae3337f6a5d468f7232b5e568a
Matthias Wirth <matthias.wirth@gmail.com>
2020-04-25 21:14:53 +0200

    adapt for use on image

--- scripts/adsbexchange-mlat.sh
+++ scripts/adsbexchange-mlat.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+if [ -f /boot/adsb-config.txt ]; then
+    source /boot/adsb-config.txt
+    source /boot/tat-env
+else
+    source /etc/default/theairtraffic
+fi
+
 /usr/local/share/theairtraffic/venv/bin/python3 /usr/local/share/theairtraffic/venv/bin/mlat-client \
 	--input-type "$INPUT_TYPE" --no-udp \
 	--input-connect "$INPUT" \
 	--server "$MLATSERVER" \
 	--user "$USER" \
-	--lat "$RECEIVERLATITUDE" \
-	--lon "$RECEIVERLONGITUDE" \
-	--alt "$RECEIVERALTITUDE" \
-	$RESULTS
+	--lat "$LATITUDE" \
+	--lon "$LONGITUDE" \
+	--alt "$ALTITUDE" \
+	$RESULTS $RESULTS1 $RESULTS2 $RESULTS3 $RESULTS4
