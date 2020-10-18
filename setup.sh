a7ab1a24c680ae86c7afb340f7c47b6a51052a9e
Matthias Wirth <matthias.wirth@gmail.com>
2020-10-18 17:00:52 +0200

    better logging for the compile

--- setup.sh
+++ setup.sh
@@ -290,9 +290,9 @@ fi
         git clone --single-branch --depth 1 https://github.com/theairtraffic/readsb.git /tmp/readsb  >> $LOGFILE 2>&1
         cd /tmp/readsb
         echo 74
-        if make -j3 AIRCRAFT_HASH_BITS=12 >> $LOGFILE >/dev/null
+        if make -j3 AIRCRAFT_HASH_BITS=12 >> $LOGFILE 2>&1
         then
-            git rev-parse HEAD > $IPATH/readsb_version 2>> $LOGFILE
+            git rev-parse HEAD > $IPATH/readsb_version >> $LOGFILE 2>&1
         fi
 
         mv $IPATH/feed-tat /tmp/old-feed-tat &>/dev/null
