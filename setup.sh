89f944f891b478fb22f81e6fe1804388c7451054
Matthias Wirth <matthias.wirth@gmail.com>
2020-12-04 17:10:19 +0100

    change text

--- setup.sh
+++ setup.sh
@@ -76,7 +76,7 @@ BACKTITLETEXT="TheAirTraffic Setup Script"
 whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yesno "Thanks for choosing to share your data with TheAirTraffic!\n\nTheAirTraffic.com is a co-op of ADS-B/Mode S/MLAT feeders from around the world. This script will configure your current your ADS-B receiver to share your feeders data with TheAirTraffic.\n\nWould you like to continue setup?" 13 78
 if [[ $? != 0 ]]; then abort; fi
 
-THEAIRTRAFFICUSERNAME=$(whiptail --backtitle "$BACKTITLETEXT" --title "Feeder MLAT Name" --nocancel --inputbox "\nPlease enter a unique name for the feeder to be shown on the MLAT matrix (http://theairtraffic.com/sync)\n\nThis name MUST be unique, for this reason a random number is automatically added at the end.\nText and Numbers only - everything else will be removed.\nExample: \"william34-london\", \"william34-jersey\", etc." 12 78 3>&1 1>&2 2>&3)
+THEAIRTRAFFICUSERNAME=$(whiptail --backtitle "$BACKTITLETEXT" --title "Feeder MLAT Name" --nocancel --inputbox "\nPlease enter a unique name for the feeder to be shown on the MLAT matrix (http://theairtraffic.com/sync)\n\nText and Numbers only - everything else will be removed.\nExample: \"william34-london\", \"william34-jersey\", etc." 12 78 3>&1 1>&2 2>&3)
 
 if [[ $? != 0 ]]; then abort; fi
 
