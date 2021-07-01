1b5f36815c679a33809875ca816fa3da166e680b
Matthias Wirth <matthias.wirth@gmail.com>
2021-07-01 11:02:15 +0200

    remove optional tar1090 instance when uninstalling

--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,10 @@ systemctl disable --now theairtraffic-mlat
 systemctl disable --now theairtraffic-mlat2 &>/dev/null
 systemctl disable --now theairtraffic-feed
 
+if [[ -d /usr/local/share/tar1090/html-tat ]]; then
+    bash /usr/local/share/tar1090/uninstall.sh tat
+fi
+
 rm -f /lib/systemd/system/theairtraffic-mlat.service
 rm -f /lib/systemd/system/theairtraffic-mlat2.service
 rm -f /lib/systemd/system/theairtraffic-feed.service
