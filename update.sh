f60cc320e9f35f8de43897c56328519cab129ca2
Matthias Wirth <matthias.wirth@gmail.com>
2022-04-12 17:19:37 +0200

    if the service fails to start, reinstall mlat-client / readsb on next run

--- update.sh
+++ update.sh
@@ -294,6 +294,7 @@ fi
 echo 94
 
 systemctl is-active theairtraffic-feed || {
+    rm -f $IPATH/readsb_version
     echo "---------------------------------"
     journalctl -u theairtraffic-feed | tail -n10
     echo "---------------------------------"
@@ -305,6 +306,7 @@ systemctl is-active theairtraffic-feed || {
 
 echo 96
 systemctl is-active theairtraffic-mlat || {
+    rm -f $IPATH/mlat_version
     echo "---------------------------------"
     journalctl -u theairtraffic-mlat | tail -n10
     echo "---------------------------------"
