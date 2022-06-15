abf21c053b6ba29d5bb8251f7508f68ac2a92e50
Matthias Wirth <matthias.wirth@gmail.com>
2022-06-15 13:41:53 +0200

    better check for /boot/adsb-config.txt

--- scripts/adsbexchange-mlat.sh
+++ scripts/adsbexchange-mlat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -f /boot/adsb-config.txt ]; then
+if grep -qs -e 'LATITUDE' /boot/adsb-config.txt &>/dev/null && [[ -f /boot/tat-env ]]; then
     source /boot/adsb-config.txt
     source /boot/tat-env
 else
