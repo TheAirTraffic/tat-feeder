9ae749bd9f63ca7775031ed3037d64bb0ef19229
Jack Sweeney <JackHSweeney1@gmail.com>
2023-02-04 20:44:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-REPO="https://github.com/theairtraffic/feedclient.git"
+REPO="https://github.com/Jxck-S/feedclient.git"
 BRANCH="master"
 IPATH=/usr/local/share/theairtraffic
 mkdir -p $IPATH
