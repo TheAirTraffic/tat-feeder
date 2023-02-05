a46acc1c90a6fa4825d7207db05e0b65c81215c3
Jack Sweeney <JackHSweeney1@gmail.com>
2023-02-04 20:53:11 -0500

    Update install-or-update-interface.sh

--- install-or-update-interface.sh
+++ install-or-update-interface.sh
@@ -23,4 +23,4 @@ GIT="/usr/local/share/tar1090/git"
 
 getGIT "$REPO" "$BRANCH" "$GIT"
 
-bash "$GIT/install.sh" "/run/theairtraffic-feed" "tat"
+bash "$GIT/install.sh" "/run/theairtraffic-feed" "theairtraffic"
